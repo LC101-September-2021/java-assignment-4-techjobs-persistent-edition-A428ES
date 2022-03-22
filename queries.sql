a23497fdb946525e4d1f55b91bf2e8d8a3d98c58
Sergio Estrada <estrada.serg@gmail.com>
2022-03-21 20:51:56 -0500

    finished with assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 -- Part 1: Test it with SQL
+SELECT column_name, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'job'
 
 -- Part 2: Test it with SQL
+SELECT * FROM employer WHERE location = "St. Louis City"
 
 -- Part 3: Test it with SQL
+DROP TABLE job
 
 -- Part 4: Test it with SQL
+SELECT name, description
+FROM techjobs.skill
+INNER JOIN techjobs.job_skills ON techjobs.skill.id = techjobs.job_skills.skills_id
+WHERE techjobs.job_skills.jobs_id IS NOT NULL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @Size(min = 3, max = 256, message = "Does not meet length requirements.")
+    private String location;
 
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
 }
